9f463e9c8ff82e79da62d0a60131e934c2ea409b
Brendan <brendanofallon@fastmail.fm>
2012-10-08 17:35:21 -0600

    Added forgotten LIBSVMTool

---
+++ src/libsvm/LIBSVMTool.java
@@ -0,0 +1,37 @@
+package libsvm;
+
+import java.io.IOException;
+
+import snpsvm.app.CommandLineApp;
+
+public class LIBSVMTool {
+
+	protected String libsvmPath = null;
+	protected boolean initialized = false;
+	
+	protected void initialize() {
+		String libsvmHome = CommandLineApp.configModule.getProperty("libsvm");
+		if (libsvmHome == null) {
+			System.err.println("Predictor can't run because it can't find libsvm. Please set the path to \"libsvm\" using config");
+		}
+		
+		libsvmPath = libsvmHome;
+		initialized = true;
+	}
+
+	protected void executeCommand(String command) {
+		ProcessBuilder procBuilder = new ProcessBuilder("bash", "-c", command);
+		System.out.println("Executing command : " + command);
+		
+		try {
+			Process proc = procBuilder.start();
+			int exitVal = proc.waitFor();
+		}
+		catch (InterruptedException ex) {
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
